cb0bf79ae9f2031e53dd640165fa12656a138c36
Vladlen Naumov <naumov.vlad04@gmail.com>
2026-07-26 12:00:00 +0300

    Add lua config

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,14 @@
+require "nvchad.options"
+
+-- add yours here!
+
+vim.opt.expandtab = false
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.mouse = 'a'
+vim.opt.mousescroll = 'ver:1,hor:6'
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
