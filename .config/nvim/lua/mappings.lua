6a41c5d6c66866d78acda1839dcf1e96ba823d2e
Labmem-015 <naumov.vlad04@gmail.com>
2026-08-05 10:27:47 +0300

    Add keymap to move lines and visual blocks up and down

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -7,4 +7,9 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+map({ "n", "i" }, "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
+map({ "n", "i" }, "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
+
+map("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+map("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
