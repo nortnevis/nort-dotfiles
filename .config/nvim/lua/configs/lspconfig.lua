6c1c0cd657436a872f0034eb349434b3c6560b81
Labmem-015 <naumov.vlad04@gmail.com>
2026-08-13 09:57:29 +0300

    Disable auto insertion of headers

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -4,6 +4,10 @@ vim.lsp.config("opencl_ls", {
   filetypes = { "cl", "lisp" }, -- lisp, because nvim-lspconfig treats .cl files as a lisp
 })
 
+vim.lsp.config("clangd", {
+  cmd = { "clangd", "--header-insertion=never" },
+})
+
 local servers = { "html", "cssls", "clangd", "opencl_ls", "stylua" }
 vim.lsp.enable(servers)
 
