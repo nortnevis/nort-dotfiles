eb29c1b48b6a50d4729604f860f3b39c00204ed5
nortnevis <naumov.vlad04@gmail.com>
2026-08-24 08:59:06 +0300

    Add monitor_configuring

---
+++ .config/hypr/nort_hypr/defaults/monitors.lua
@@ -0,0 +1,6 @@
+hl.monitor({
+	output = "",
+	mode = "",
+	position = "",
+	scale = 1,
+})
--- .config/hypr/nort_hypr/hyprland.lua
+++ .config/hypr/nort_hypr/hyprland.lua
@@ -1,11 +1,11 @@
 ----- НАСТРОЙКА МОНИТОРОВ ------------------------------------------------------
+require("nort_hypr.defaults.monitors")
 
-hl.monitor({
-	output = "HDMI-A-1",
-	mode = "1920x1080@60",
-	position = "0x0",
-	scale = 1,
-})
+local success, err = pcall(require, "nort_hypr.user_defined.monitors")
+
+if success then
+	print("[INFO] Successfully loaded nort_hypr.user_defined.monitors")
+end
 
 hl.config({
 	xwayland = {
